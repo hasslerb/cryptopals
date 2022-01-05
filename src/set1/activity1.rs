1866ce1d86fcfa7d3e9e2531e37ff7eec5b0143d
Bret <bhassler@punchthrough.com>
2022-01-05 10:00:43 -0600

    Challenge 1

--- src/set1/activity1.rs
+++ src/set1/activity1.rs
@@ -1,3 +1,16 @@
+// Convert hex to base64
+// The string:
+// 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+// Should produce:
+// SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+// So go ahead and make that happen. You'll need to use this code for the rest of the exercises.
+
+use hex;
+use base64;
+
 fn main() {
-    println!("Hello, world! Activity1");
+    let input: String = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    let raw = hex::decode(input).expect("Failed to decode input");
+    let output = base64::encode(raw);
+    assert_eq!(output, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
 }
